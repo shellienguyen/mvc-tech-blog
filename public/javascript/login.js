5a5a60a192152c752500561f69db2a5f9b55b757
sidonie <shelliednnguyen@gmail.com>
2021-03-21 21:55:44 -0700

    javascript form handler files

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,33 +1,24 @@
-// Login Submission
-async function loginFormHandler(event) {
+async function loginFormHandler( event ) {
    event.preventDefault();
 
-   const username = document.querySelector('#username-login').nodeValue.trim();
-   const password = document.querySelector('#password-login').nodeValue.trim();
+   const username = document.querySelector( '#username-login' ).nodeValue.trim();
+   const password = document.querySelector( '#password-login' ).nodeValue.trim();
 
-   console.log( 'username: ');
-   console.log(username);
-   console.log('password');
-   console.log(password);
-
-   if (username && password) {
-      const response = await fetch('/api/users/login', {
+   if ( username && password ) {
+      const response = await fetch( '/api/users/login', {
          method: 'post',
-         body: JSON.stringify({
-            username,
-            password
-         }),
-         headers: {'Content-Type': 'application/json'}
+         body: JSON.stringify({ username, password }),
+         headers: { 'Content-Type': 'application/json' }
       });
 
-      if (response.ok) {
-         document.location.replace('/dashboard');
+      if ( response.ok ) {
+         document.location.replace( '/dashboard' );
       }
       else {
-         alert(response.statusText);
+         alert( response.statusText );
       };
    };
 };
 
 
-document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
+document.querySelector( '#login-form' ).addEventListener( 'submit', loginFormHandler );
